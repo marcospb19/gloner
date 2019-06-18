ba1c606d35f8ea96c44993b6fe263af6b42d64da
João Marcos <marcospb19@hotmail.com>
2019-06-18 16:40:15 -0300

    Added show_help and help_command functions

--- src/functions.sh
+++ src/functions.sh
@@ -1,2 +1,28 @@
 # functions.sh
-#
+
+function show_help()
+{
+	echo "Usage: gloner [-h | --help] [-V | --version] [-v | --verbose]"
+	echo
+	echo "Use command help command and flags description:"
+	echo "    gloner help [Command]"
+	echo
+	echo "Commands:"
+
+	# echo "          help  <Command>
+	# echo "          clone <User> [Repository] [Target]
+	exit 0
+}
+
+function help_command()
+{
+	echo "This is the help command, it's pending."
+	# Show described information about each command, shown when
+	# gloner help <Command>
+
+	# Format for later Usage: "gloner [Command_Here!!] [Args...]"
+	# Use this format for all the help command descriptions
+
+	# Is typed, and also when a bad usage is detected from a known command
+	exit 0
+}
