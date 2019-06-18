2437b2cbbef80fe875804bd24352261d07b85227
João Marcos <marcospb19@hotmail.com>
2019-06-18 20:25:44 -0300

    Filled header.sh

--- src/header.sh
+++ src/header.sh
@@ -1,2 +1,15 @@
 # header.sh
-# This is the header file with copyright content
+
+#          __
+#    ___ _/ /__  ___  ___ ____
+#   / _ `/ / _ \/ _ \/ -_) __/
+#   \_, /_/\___/_//_/\__/_/
+#  /___/
+#
+#
+#  Version 0.0
+#
+#  Licensed under MIT.
+#  From https://github.com/marcospb19/gloner, feel free to report your
+#  issues and feature requests, as well as contributing in other ways :)
+#
