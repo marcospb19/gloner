18a4b4bfdd44bf9e548ff5acfe69cf05c8e9b744
João Marcos <marcospb19@hotmail.com>
2019-06-22 01:28:52 -0300

    Finished parse_arguments function

--- src/parse.sh
+++ src/parse.sh
@@ -1,24 +1,47 @@
 # parse.sh
 
 # Only receives one string variable starting with a hyphen
+# representing a flag (Example: --help or -h)
 function parse_flags()
 {
-	# If second character is also a hyphen
-	# Example: --help
-	if [[ "${1:1:1}" ]]; then
+	local input="$1"
 
+	if [[ "${input:1:1}" == "-" ]]; then
+		# If second character is also a hyphen
+		# Example: --help
+
+		local arg="${input:2}"
+
+		case "$arg" in
+
+			help)    show_help    ; exit 0 ;;
+			version) show_version ; exit 0 ;;
+
+			verbose) verbose="true" ;;
+			http*)   http="true"    ;;
+			ssh)     ssh="true"     ;;
+
+			*) echo "Option '-$arg' not recognized, see gloner --help."
+				exit 1 ;;
+		esac
 	else
-	# Only first character is a hyphen
-	# Example -h
-		local size="${#var}"
+		# Only first character is a hyphen
+		# Example -h
+		local size="${#1}"
 		local arg=""
+
+		# For each letter after the hyphen, process the flags
+		# Example: "-hv", process flags 'h' and 'v'
 		for (( i = 1 ; i < $size ; i++ )); do
-			arg="${var:$i:1}"
+			arg="${input:$i:1}"
 
 			case "$arg" in
-				# h) show_help
-					# exit 0 ;;
-				*) echo "Option '-$arg' not recognized, see gloner --help."
+				h) show_help    ; exit 0 ;;
+				V) show_version ; exit 0 ;;
+
+				v) verbose="true"        ;;
+
+				*) echo "Unknown option '-$arg', see gloner --help."
 					exit 1 ;;
 			esac
 
@@ -29,15 +52,14 @@ function parse_flags()
 # Receives all arguments passed to gloner
 function parse_arguments()
 {
-	arguments=$()
+	arguments=()
+
 	for i in "$@"; do
 		# Check if first character is a hyphen
 		if [[ "${i:0:1}" == "-" ]]; then
 			parse_flags "$i"
-
-		# else
-			# append arguments...
-
+		else
+			arguments+=("$i")
 		fi
 	done
 }
