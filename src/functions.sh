bac26394676d8fdcf4541f98f1614082b6749a02
João Marcos <marcospb19@hotmail.com>
2019-07-02 00:26:57 -0300

    Moving help functions from functions.sh to help.sh

--- src/functions.sh
+++ src/functions.sh
@@ -13,29 +13,3 @@ function show_version()
 	echo "From https://github.com/marcospb19/gloner, feel free to report your"
 	echo "issues and feature requests, as well as contributing in other ways :)"
 }
-
-function show_help()
-{
-	echo "Usage: gloner [-h | --help] [-V | --version] [-v | --verbose]"
-	echo
-	echo "Use command help command and flags description:"
-	echo "    gloner help [Command]"
-	echo
-	echo "Commands:"
-
-	# echo "          help  <Command>
-	# echo "          clone <User> [Repository] [Target]
-}
-
-function help_command()
-{
-	echo "This is the help command, it's pending."
-	# Show described information about each command, shown when
-	# gloner help <Command>
-
-	# Format for later Usage: "gloner [Command_Here!!] [Args...]"
-	# Use this format for all the help command descriptions
-
-	# Is typed, and also when a bad usage is detected from a known command
-	exit 0
-}
--- src/help.sh
+++ src/help.sh
@@ -1,1 +1,28 @@
 # help.sh
+
+function show_help()
+{
+	echo "Usage: gloner [-h | --help] [-V | --version] [-v | --verbose]"
+	echo
+	echo "Use command help command and flags description:"
+	echo "    gloner help [Command]"
+	echo
+	echo "Commands:"
+
+	# echo "          help  <Command>
+	# echo "          clone <User> [Repository] [Target]
+}
+
+# The help command describes any other command
+function help()
+{
+	echo "This is the help command, it's pending."
+	# Show described information about each command, shown when
+	# gloner help <Command>
+
+	# Format for later Usage: "gloner [Command_Here!!] [Args...]"
+	# Use this format for all the help command descriptions
+
+	# Is typed, and also when a bad usage is detected from a known command
+	exit 0
+}
