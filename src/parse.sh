716a56777e84a8ab9cfde981f9abaac459c0ec1d
João Marcos <marcospb19@hotmail.com>
2019-07-13 21:24:51 +0000

    Added function check_git_directories

--- src/parse.sh
+++ src/parse.sh
@@ -96,3 +96,30 @@ function check_directories()
 		exit 1
 	fi
 }
+
+# Similar to check_directories
+#
+# Checks if array of arguments are valid git
+# directories, if one of them isn't, exit
+function check_git_directories()
+{
+	exit=""
+
+	for i in "$@"; do
+		# If isn't a directory
+		if [[ ! -d "$i" ]]; then
+			exit="true"
+			echoerr "'$(realpath "$i")' is not a directory"
+
+		# If isn't a git directory
+		elif [[ ! -d "$i"/.git ]]; then
+			exit="true"
+			echoerr "\"$(realpath "$i")\" is not a git directory"
+		fi
+	done
+
+	# Note that this function lists every fail before exiting
+	if [[ "$exit" == "true" ]]; then
+		exit 1
+	fi
+}
