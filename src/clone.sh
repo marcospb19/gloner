535109d981a88caedecffaa8b3a7d7277cb69300
João Marcos <marcospb19@hotmail.com>
2019-07-13 21:35:34 +0000

    Added clone_respository funcion

--- src/clone.sh
+++ src/clone.sh
@@ -1,1 +1,26 @@
 # clone.sh
+
+function clone_repository()
+{
+	if [[ "$#" == 0 ]]; then
+		echoerr "No arguments given."
+		exit 1
+
+	elif [[ "$#" != 2 && "$#" != 3 ]]; then
+		echoerr "gloner clone: expected 1 or 2 arguments, but received $(( $# - 1 ))."
+		exit 1
+	fi
+
+	local       user="$1"
+	local repository="$2"
+	local     target="$3"
+
+	# Decide which protocol to use
+	local clone_url
+
+	clone_url="https://github.com/$user/$repository"
+
+	# Flags can't be quoted here, because they're optional
+	echo "git clone $clone_url $target $flagR $flagv"
+	git clone $clone_url $target $flagR $flagv
+}
