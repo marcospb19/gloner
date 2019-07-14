6ee38c7dbf871c0e66c456449f02f858772166c3
João Marcos <marcospb19@hotmail.com>
2019-07-14 01:13:46 +0000

    Help command and command list added

--- src/help.sh
+++ src/help.sh
@@ -1,28 +1,77 @@
 # help.sh
 
-function show_help()
+function list_available_commands()
 {
-	echo "Usage: gloner [-h | --help] [-V | --version] [-v | --verbose]"
-	echo
-	echo "Use command help command and flags description:"
-	echo "    gloner help [Command]"
-	echo
-	echo "Commands:"
+	cat <<- "EOT"
 
-	# echo "          help  <Command>
-	# echo "          clone <User> [Repository] [Target]
+		Available commands:
+
+		    Help:
+		        help  <Command>                      [-v|--verbose]
+
+		    Clone:
+		        clone  <User> [Repository] [Target]  [--ssh|--http] [-R|--recursive]
+
+		    Remote:
+		        geturl  <Folders>                    [-v|--verbose] [-R|--recursive]
+
+	EOT
 }
 
-# The help command describes any other command
-function help()
+function show_help()
 {
-	echo "This is the help command, it's pending."
-	# Show described information about each command, shown when
-	# gloner help <Command>
+	cat <<- "EOT"
 
-	# Format for later Usage: "gloner [Command_Here!!] [Args...]"
-	# Use this format for all the help command descriptions
+		Usage: gloner <Command> [-h|--help] [-V|--version] [-v|--verbose] [-R|--recursive]
 
-	# Is typed, and also when a bad usage is detected from a known command
+	EOT
+
+	list_available_commands
+}
+
+# Just a function wrapper
+function show_help_and_exit()
+{
+	echo
+	show_help
+	exit 1
+}
+
+# Show described information about other command, called when
+# "gloner help <Command>" is typed, or when a bad usage is detected
+# inside of an command function
+function help_command()
+{
+	if [[ "$#" == 0 ]]; then
+		echoerr "No arguments given."
+		exit 1
+	fi
+
+	local command="$1"
+
+	if [[ "$command" == "help" ]]; then
+		echo "gloner help - Show help and usage to any gloner command"
+		echo
+		echo "Usage:"
+		echo "    gloner help <Command>"
+
+	elif [[ "$command" == "clone" ]]; then # Pending
+		echo "gloner clone - Clone a repository only by having the user and name"
+		echo
+		echo "Usage:"
+		echo "    gloner clone <User> <Repository> [Target]"
+
+	elif [[ "$command" == "geturl" ]]; then
+		echo "gloner geturl - Show the remote URL (origin) from local repositories"
+		echo
+		echo "Usage:"
+		echo "    gloner geturl <Folders> [--verbose]"
+
+	else
+		echoerr "\"$command\" command not recognized."
+		list_available_commands
+		exit 1
+	fi
+
 	exit 0
 }
